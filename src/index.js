5338404075fee256642c07ce86df5f7892fc6bb0
Zsolt Tóth <waysofdarkness@gmail.com>
2023-05-28 14:09:39 +0200

    Added means of converting to my own MediaWiki format.

--- src/index.js
+++ src/index.js
@@ -34,6 +34,42 @@ function decrementDate(date, distance) {
   newDate.setMinutes(newDate.getMinutes() - distance.minutes);
   return newDate;
 }
+function bbcodeUrlToHtml(text) {
+  const urlPattern = /\[url=([^[\]]+)\]([^[\]]+)\[\/url\]/g;
+  const urlReplacement = '<a href="$1">$2</a>';
+
+  return text.replace(urlPattern, urlReplacement);
+}
+function bbcodeToHtml(bbcode) {
+  const tags = [
+    { bbcode: /\[b\]/g, html: '<strong>' },
+    { bbcode: /\[\/b\]/g, html: '</strong>' },
+    { bbcode: /\[i\]/g, html: '<em>' },
+    { bbcode: /\[\/i\]/g, html: '</em>' },
+    { bbcode: /\[u\]/g, html: '<u>' },
+    { bbcode: /\[\/u\]/g, html: '</u>' },
+  ];
+
+  let html = bbcode;
+
+  tags.forEach((tag) => {
+    html = html.replace(tag.bbcode, tag.html);
+  });
+  html = bbcodeUrlToHtml(html);
+  html = html.replace(/\r?\n/g, "<br>");
+
+  return html;
+}
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+}
 function processHTMLFile(rpgPath) {
 	const stats = fs.statSync(rpgPath);
 	const fileDate = stats.mtime;
@@ -87,10 +123,36 @@ function processHTMLFile(rpgPath) {
 			delete obj.relativeDate;
 		}
 	});
+	finalOutput.posts.forEach(function(currentValue, index) {
+		if(currentValue.date != null) {
+			currentValue.date.setSeconds(currentValue.date.getSeconds() + index);
+		}
+	});
 	const json = JSON.stringify(finalOutput);
 	const jsonFileName = path.basename(rpgPath, path.extname(rpgPath)) + '.json';
     const jsonFilePath = path.join(path.dirname(rpgPath), jsonFileName);
 	fs.writeFileSync(jsonFilePath,json);
+	/*const mappedText = finalOutput.posts.map(({sender,date,longerText}) => {
+            return (
+                `{{RPG Post/${sender}
+|date=${formatDate(date)}
+|post=${bbcodeToHtml(longerText)}
+}}`
+                )
+            }).join("\n");
+	console.log(mappedText);*/
+	const textFileName = path.basename(rpgPath, path.extname(rpgPath)) + '.txt';
+    const textFilePath = path.join(path.dirname(rpgPath), textFileName);
+    fs.writeFileSync(textFilePath, finalOutput.posts.map(({sender,date,longerText}) => {
+            return (
+                `{{RPG Post/${sender}
+|date=${formatDate(date)}
+|post=${bbcodeToHtml(longerText)}
+}}`
+                )
+            }).join("\n")
+    );
+	
 }
 const rpgPath = process.argv[2];
 console.log(rpgPath);
